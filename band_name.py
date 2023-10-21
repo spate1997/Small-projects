a02aa15290c741b0deaa7616fbe2a8c951b8f111
spate1997 <86443652+spate1997@users.noreply.github.com>
2023-10-21 14:52:12 -0400

    Update band_name.py

--- band_name.py
+++ band_name.py
@@ -1,3 +1,4 @@
+//This code is made to give suggestions for when creating a band name. 
 print ("Welcome to the band creator")
 city = input("What City did you grow up in?")
 pet = input ("What is the name of your pet?")
