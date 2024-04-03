b227e91f3ea6daac429511a50dfb72e90e32ea49
spate1997 <86443652+spate1997@users.noreply.github.com>
2024-04-03 14:37:36 -0400

    Add files via upload

---
+++ bmi.rb
@@ -0,0 +1,21 @@
+def bmi(weight, height)
+    bmi = weight / height**2
+    if bmi <= 18.5
+      "Underweight"
+    elsif bmi <= 25.0
+      "Normal"
+    elsif bmi <= 30.0
+      "Overweight"
+    elsif bmi > 30
+      "Obese"
+    end
+  end
+  
+  puts "Enter weight in kilograms:"
+  weight = gets.chomp.to_f  # Converts input to a floating point number, suitable for weight
+  
+  puts "Enter height in meters:"
+  height = gets.chomp.to_f  # Converts input to a floating point number, suitable for height
+  
+  puts "BMI Category: #{bmi(weight, height)}"
+  
