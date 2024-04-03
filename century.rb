260833caf0bc0dfc0cacf197634d0bc6c685fa89
spate1997 <86443652+spate1997@users.noreply.github.com>
2024-04-03 14:34:02 -0400

    Add files via upload

---
+++ century.rb
@@ -0,0 +1,9 @@
+def century(year)
+    (year + 99) / 100
+  end
+  
+  puts "Enter year:"
+  year = gets.chomp.to_i  # Reads input from the user, removes the newline, and converts to an integer
+  
+  puts "The year #{year} is in the #{century(year)} century."
+  
