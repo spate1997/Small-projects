3fd2836868e0fec57d1a1a3af1882c64be4c93fd
Sheel Patel <86443652+spate1997@users.noreply.github.com>
2024-06-14 20:21:22 -0400

    Update library.py

---
+++ library.py
@@ -0,0 +1,21 @@
+# Example of Book class implementation
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.is_borrowed = False
+
+    def borrow(self):
+        if not self.is_borrowed:
+            self.is_borrowed = True
+            return True
+        return False
+
+    def return_book(self):
+        if self.is_borrowed:
+            self.is_borrowed = False
+            return True
+        return False
+
+# Implement the Member and Library classes similarly
