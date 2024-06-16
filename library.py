7cc4692f4e408f03e987bdc6b72ffa42b17b397b
Sheel Patel <86443652+spate1997@users.noreply.github.com>
2024-06-16 15:29:15 -0400

    Created Member class

--- library.py
+++ library.py
@@ -18,4 +18,21 @@ class Book:
             return True
         return False
 
-# Implement the Member and Library classes similarly
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = []
+
+    def borrow_book(self, book):
+        if book.borrow():
+            self.borrowed_books.append(book)
+            return True
+        return False
+
+    def return_book(self, book):
+        if book in self.borrowed_books and book.return_book():
+            self.borrowed_books.remove(book)
+            return True
+        return False
+#to do add Library class
