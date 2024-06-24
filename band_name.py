d4f534f218627ac6272cf4e20440d8b91fb26ca8
Sheel Patel <86443652+spate1997@users.noreply.github.com>
2024-06-23 23:36:48 -0400

    Update band_name.py

--- band_name.py
+++ band_name.py
@@ -3,3 +3,4 @@ print ("Welcome to the band creator")
 city = input("What City did you grow up in?")
 pet = input ("What is the name of your pet?")
 print ("your band name should be "+ city +" "+ pet)
+#
